200aafb61f2bfd364728ba7181d86e347b1b7fc6
Hardik <hardikbharunt@gmail.com>
2019-05-22 11:46:00 +0530

    Flask App- Updated

--- FlaskApp/Blog.py
+++ FlaskApp/Blog.py
@@ -3,7 +3,15 @@ from flask import Flask, render_template # Import Flask
 app = Flask(__name__) # Initialize Flask App
 
 @app.route('/') 
-def homepage():
-    return render_template("home.html") 
+def home():
+    return render_template("home.html")
+
+@app.route('/blog') 
+def blog():
+    return render_template("blog.html") 
+
+@app.route('/contactus') 
+def contactus():
+    return render_template("contactus.html") 
 
 app.run( host='0.0.0.0' , port=5000 )
